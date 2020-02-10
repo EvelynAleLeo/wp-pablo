8318c51aa5c6b68a8238ea1495bfc5f4acfab253
Josue Estevez Fernandez <josu@josues-mbp.home>
2020-02-10 00:16:02 +0000

    Added Facebook and Instagram icons with links

--- footer.php
+++ footer.php
@@ -2,8 +2,13 @@
     </main><!-- #site-content -->
     <footer>
       <div class="media">
-      HOLA
+        <a href="https://www.facebook.com/pablogonzalezrestauracion/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
+        <a href="https://www.instagram.com/mostrarte_cortes/" target="_blank"><i class="fab fa-instagram-square fa-2x"></i></a>
+        <!-- <i class="fab fa-youtube-square fa-2x"></i> -->
+        <!-- <i class="fab fa-linkedin fa-2x"></i> -->
       </div>
     </footer>
+    <!-- TODO: probably better to download SVGs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
   </body>
 </html>
